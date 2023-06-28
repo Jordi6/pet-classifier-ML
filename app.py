b44198cf802758c22a72a6cbfa5ef28d2d758491
tonystark <jordybecerril@gmail.com>
2023-06-27 21:23:59 -0500

    pet classifier!

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
+
+from fastai.vision.all import *
 import gradio as gr
+import timm
 
-def greet(name):
-    return "Hello " + name + "!!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+learn = load_learner('model.pkl')
+
+categories = learn.dls.vocab
+
+def classify_image(img):
+    pred,idx,probs = learn.predict(img)
+    return dict(zip(categories, map(float,probs)))
+
+
+image = gr.inputs.Image(shape=(192,192))
+label = gr.outputs.Label()
+examples = ['basset.jpg']
+
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, exmples=examples)
+intf.launch()
